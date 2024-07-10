d7e6859db3ee82c0f9849433145c9b1471f1920f
Liene Briede <lienebriede84@gmail.com>
2024-07-10 14:51:26 +0000

    Update Add-product functionality

--- project-root/public/js/script.js
+++ project-root/public/js/script.js
@@ -1,31 +1,33 @@
-// Type switcher function
-document.getElementById('productType').addEventListener('change', function () {
-    switchType.call(this);
-});
-
-function switchType() {
-    const type = this.value;
-    const types = ['Book', 'DVD', 'Furniture'];
-
-    types.forEach(function (item) {
-        const element = document.getElementById(item);
-        if (item === type) {
-            element.classList.remove('d-none');
-            element.classList.add('d-block');
-        } else {
-            element.classList.remove('d-block');
-            element.classList.add('d-none');
-        }
-    });
-}
-
-// Product Add
 document.addEventListener('DOMContentLoaded', function () {
+
+    /** Type switcher function */
+    document.getElementById('productType').addEventListener('change', function () {
+        switchType.call(this);
+    });
+
+    function switchType() {
+        const type = this.value;
+        const types = ['Book', 'DVD', 'Furniture'];
+
+        types.forEach(function (item) {
+            const element = document.getElementById(item);
+            if (item === type) {
+                element.classList.remove('d-none');
+                element.classList.add('d-block');
+            } else {
+                element.classList.remove('d-block');
+                element.classList.add('d-none');
+            }
+        });
+    }
 
     /** Form data validation */
     function validateForm() {
         let isValid = true;
         const requiredFields = document.querySelectorAll('#product_form [required]');
+
+        const notifications = document.querySelectorAll('.notification');
+        notifications.forEach(notification => notification.remove());
 
         requiredFields.forEach((field) => {
             if (!field.value.trim()) {
